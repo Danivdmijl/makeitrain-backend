9eab05bab7a713c8e415ceb2667405727d72086f
Danivdmijl <32570@ma-web.nl>
2022-05-30 10:16:53 +0200

    PDF Design/Begin video 2

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require 'functions.php';
+$connection = dbConnect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <article>
+        <h2>Dit is de titel</h2>
+        <p>P tag</p>
+        <img src="" alt="">
+        <h3>h3</h3>
+        <h3>nog een h3</h3>
+    </article>
+
+</body>
+</html>
